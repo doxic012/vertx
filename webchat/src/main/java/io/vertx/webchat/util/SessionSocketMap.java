d1b7778785ec5586238d1484ebcc8bdc9c0fd439
Stefan Fischer <doxic012@gmail.com>
2015-06-05 12:25:46 +0200

    MessageHandler gelöscht, SessionSocketMap hinzugefügt (Session, WebSocket-liste), SessionSocketMap im WebSocketManager implementiert

---
+++ webchat/src/main/java/io/vertx/webchat/util/SessionSocketMap.java
@@ -0,0 +1,84 @@
+package io.vertx.webchat.util;
+
+import io.vertx.core.http.ServerWebSocket;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.apex.Session;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.function.Function;
+
+public final class SessionSocketMap extends HashMap<Session, List<ServerWebSocket>> {
+
+	/**
+	 * Verknüpfe WebSocket mit Apex-Session.
+	 * 
+	 * @param session
+	 * @param socket
+	 */
+	public void add(Session session, ServerWebSocket socket) {
+		computeIfAbsent(session, list -> new ArrayList<>()).add(socket);
+	}
+
+	/**
+	 * Entfernt einen ServerWebSocket aus der Liste einer Session. Wenn die Liste leer ist, wird die Session aus der
+	 * HashMap entfernt
+	 * 
+	 * @param session
+	 * @param socket
+	 */
+	public void remove(Session session, ServerWebSocket socket) {
+		if (!this.containsKey(session))
+			return;
+
+		List<ServerWebSocket> socketList = this.get(session);
+
+		if (socketList.contains(socket))
+			socketList.remove(socket);
+
+		if (socketList.size() == 0)
+			this.remove(session);
+	}
+
+	/**
+	 * Überprüft, ob eine Session existiert, in der ein bestimmer principalKey enthalten ist.
+	 * 
+	 * @param principal Das JsonObject, welches überprüft werden soll
+	 * @return
+	 */
+	public boolean containsPrincipal(JsonObject principal) {
+		// compare each session user with passed principal
+		for (Session session : keySet()) {
+			if (principal.equals(session.getPrincipal()))
+				return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Filtert alle ServerWebSockets anhand der übergebenen filterFunction.
+	 * Nimmt die filterFunction den Wert true an, so werden alle ServerWebSockets des entsprechenden JsonObjects der
+	 * Session übergeben, andernfalls nicht
+	 * 
+	 * @param filterFunction Die filter-funktion, welche für jede Session den entsprechende principal (JsonObject)
+	 *            übergibt.
+	 * @return
+	 */
+	public List<ServerWebSocket> filter(Function<JsonObject, Boolean> filterFunction) {
+		List<ServerWebSocket> userSockets = new ArrayList<>();
+
+		forEach((session, socketList) -> {
+			if (filterFunction.apply(session.getPrincipal()))
+				userSockets.addAll(socketList);
+		});
+
+		return userSockets;
+	}
+	
+	public List<ServerWebSocket> getUserSockets(JsonObject principal) {
+		return filter(json -> {
+			return json.equals(principal);
+		});
+	}
+}
--- webchat/src/main/java/io/vertx/webchat/util/WebSocketManager.java
+++ webchat/src/main/java/io/vertx/webchat/util/WebSocketManager.java
@@ -11,7 +11,9 @@ import io.vertx.ext.apex.Session;
 import io.vertx.webchat.mapper.ContactMapper;
 import io.vertx.webchat.util.WebSocketMessage.MessageType;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * This class handles the actual ServerWebSocket with a vertx-context.
@@ -24,9 +26,47 @@ public class WebSocketManager {
 	private Session session;
 	private ServerWebSocket socket = null;
 
-	private static HashMap<ServerWebSocket, String> userMap = new HashMap<ServerWebSocket, String>();
+	private static SessionSocketMap userMap = new SessionSocketMap();
 
 	private static HashMap<MessageType, Handler<WebSocketMessage>> socketEvents = new HashMap<MessageType, Handler<WebSocketMessage>>();
+
+	public WebSocketManager(ServerWebSocket ws, Session session) throws Exception {
+
+		// TODO: correct exception type
+		if (session == null || ws == null) {
+			if (ws != null)
+				ws.reject();
+
+			throw new Exception("Missing or invalid arguments for WebSocketManager");
+		}
+
+		this.session = session;
+		this.socket = ws;
+	
+		// Principal enthält JsonObject aus User-Model (uid, email, username)
+		JsonObject currentUser = session.getPrincipal();
+		
+		// Benutzer ist noch nicht online -> Verschicke Statusnachricht an alle anderen
+		if(!userMap.containsPrincipal(currentUser))
+			broadcastMessage(new WebSocketMessage(MessageType.USER_STATUS_ONLINE, currentUser));
+		
+		// Verknüpfe Benutzer mit Websocket (welche E-Mail gehört zu welchem Websocket) als Hashmap
+		userMap.add(session, ws);
+		
+		log.debug("registering new connection with id: " + socket.textHandlerID() + " for user: " + session.getPrincipal().getString("email") + ", users online: " + userMap.size());
+
+		socket.closeHandler(getCloseHandler());
+		socket.frameHandler(getFrameHandler());
+
+		// Verschicke Benutzerobjekt und Kontaktliste
+		writeMessage(new WebSocketMessage(MessageType.USER_DATA, currentUser));
+		writeMessage(new WebSocketMessage(MessageType.CONTACT_LIST, ContactMapper.getContacts(currentUser.getInteger("uid"))));
+	}
+	
+	
+	public SessionSocketMap getUserConnections() {
+		return userMap;
+	}
 
 	/**
 	 * Nachrichten vom Client abhandeln (socket.send)
@@ -35,16 +75,18 @@ public class WebSocketManager {
 	 */
 	private Handler<WebSocketFrame> getFrameHandler() {
 		return frame -> {
-			//TODO Alle Sockets des Benutzers schließen
-			if (session.isDestroyed() || !session.isLoggedIn()) { //Wenn Session ausgelaufen oder Benutzer ausgeloggt, socket schließen
+			// TODO Alle Sockets des Benutzers schließen
+			// Wenn Session ausgelaufen oder Benutzer ausgeloggt, socket schließen
+			if (session.isDestroyed() || !session.isLoggedIn()) { 
 				log.error("session destroyed, rejecting socket");
 				socket.close();
 				return;
 			}
 
 			try {
-				//frame.textData = message, Java und Javascript haben dasselbe Klassenobjekt WebSocketMessage, dadurch kann die Funktion Json.decodeValue die message in WebSocketMessage 
-				//deserialisieren und integrieren
+				// frame.textData = message, Java und Javascript haben dasselbe Klassenobjekt WebSocketMessage, dadurch
+				// kann die Funktion Json.decodeValue die message in WebSocketMessage
+				// deserialisieren und integrieren
 				WebSocketMessage message = Json.decodeValue(frame.textData(), WebSocketMessage.class);
 				message.setOrigin(session.getPrincipal().getString("email"));
 
@@ -67,17 +109,28 @@ public class WebSocketManager {
 	 */
 	private Handler<Void> getCloseHandler() {
 		return handler -> {
-			userMap.remove(socket);
-			//TODO Wenn Benutzer 2 Browserfenster verwendet, ist Status evtl. nicht "offline"
+			
+			// Session ist abgelaufen oder Benutzer hat sich ausgeloggt
+			// Dann broadcast an alle anderen Verbindungen
+			if(session.isDestroyed() || !session.isLoggedIn()) {
+				userMap.remove(session);
+			}
+			else {
+				userMap.remove(session, socket);
+			}
+			
+			// Wenn keine Session des Benutzers offen, dann Broadcast mit offline-status an alle 
+			if(userMap.containsPrincipal(session.getPrincipal()))
 			broadcastMessage(new WebSocketMessage(MessageType.USER_STATUS_OFFLINE, session.getPrincipal()));
 
 			log.debug("un-registering new connection with id: " + socket.textHandlerID() + " for user: " + session.getPrincipal().getString("email") + ", users online:" + userMap.size());
 		};
 	}
-	
+
 	/**
 	 * Füge Event zur Hashmap socketEvents hinzu, damit FrameHandler damit arbeiten kann
 	 * Pro MessageType nur 1 Event (deswegen replace)
+	 * 
 	 * @param type
 	 * @param handler
 	 */
@@ -87,66 +140,54 @@ public class WebSocketManager {
 		else
 			socketEvents.replace(type, handler);
 	}
-	
+
 	/**
-	 * Schickt Message an alle Benutzer/Websockets außer dem Aktuellen
+	 * Schickt Message an alle Benutzer/Websockets, die nicht mit dem außer dem aktuellen Benutzer
+	 * 
 	 * @param message
 	 */
 	public void broadcastMessage(WebSocketMessage message) {
 		JsonObject currentUser = session.getPrincipal();
 
-		userMap.forEach((socket, email) -> {
-			if (!email.equals(currentUser.getString("email"))) {
-				writeMessage(socket, message);
-			}
+		userMap
+		.filter(json -> {
+			return !json.equals(currentUser);
+		})
+		.forEach(socket -> {
+			writeMessage(socket, message);
 		});
 	}
-	
-	/**
-	 * Verschicke WebsocketMessage an aktuellen Websocket
-	 * @param msg
-	 */
-	public void writeMessage(WebSocketMessage msg) {
-		this.socket.writeFrame(msg.toFrame());
-	}
-	
+
+
 	/**
 	 * Verschicke WebsocketMessage an übergebenen Websocket (writeFrame informiert Observer über neue Daten)
+	 * 
 	 * @param socket
-	 * @param msg
+	 * @param message
 	 */
-	public void writeMessage(ServerWebSocket socket, WebSocketMessage msg) {
-		socket.writeFrame(msg.toFrame());
+	public void writeMessage(ServerWebSocket socket, WebSocketMessage message) {
+		socket.writeFrame(message.toFrame());
 	}
 
-	public WebSocketManager(ServerWebSocket ws, Session session) throws Exception {
-
-		// TODO: correct exception type
-		if (session == null || ws == null) {
-			if (ws != null)
-				ws.reject();
-
-			throw new Exception("Missing or invalid arguments for WebSocketManager");
-		}
-
-		this.session = session;
-		this.socket = ws;
-		//Principal enthält JsonObject aus User-Model (uid, email, username)
-		JsonObject currentUser = session.getPrincipal();
-
-		// Verknüpfe Benutzer mit Websocket (welche E-Mail gehört zu welchem Websocket) als Hashmap
-		userMap.put(socket, currentUser.getString("email")); 
-
-		log.debug("registering new connection with id: " + socket.textHandlerID() + " for user: " + session.getPrincipal().getString("email") + ", users online: " + userMap.size());
-
-		socket.closeHandler(getCloseHandler());
-		socket.frameHandler(getFrameHandler());
-
-		writeMessage(new WebSocketMessage(MessageType.USER_DATA, currentUser));
-		writeMessage(new WebSocketMessage(MessageType.CONTACT_LIST, ContactMapper.getContacts(currentUser.getInteger("uid"))));
-
-		// broadcast online status to everyone except us
-		broadcastMessage(new WebSocketMessage(MessageType.USER_STATUS_ONLINE, currentUser));
+	/**
+	 * Verschicke WebsocketMessage an aktuellen Websocket
+	 * 
+	 * @param message
+	 */
+	public void writeMessage(WebSocketMessage message) {
+		writeMessage(socket, message);
 	}
 
+	/**
+	 * Verschicke WebsocketMessage an alle WebSockets eines Principals
+	 * (writeFrame informiert alle Observer über neue Daten)
+	 * 
+	 * @param socket
+	 * @param message
+	 */
+	public void writeMessageToPrincipal(JsonObject principal, WebSocketMessage message) {
+		userMap.getUserSockets(principal).forEach(socket -> {
+			writeMessage(socket, message);
+		});
+	}
 }
